417551d5c191a9137cc93110d32a94a5885be262
Tymoteusz Jankowski <tymoteusz.jankowski@gmail.com>
2016-05-06 08:34:07 +0200

    review fixes

--- powerdns/tests/test_api.py
+++ powerdns/tests/test_api.py
@@ -65,6 +65,7 @@ class BaseApiTestCase(TestCase):
         self.super_user = get_user_model().objects.create_superuser(
             'super_user', 'test@test.test', 'super_user'
         )
+        self.client = APIClient()
 
 
 from django.core.urlresolvers import reverse
@@ -73,19 +74,17 @@ from rest_framework.test import APITestCase
 from powerdns.models.powerdns import Record
 from powerdns.tests.utils import DomainFactory, user_client
 from powerdns.models.requests import RecordRequest
-import json
 class TestAddingRecords(BaseApiTestCase):
     def setUp(self):
         super().setUp()
-        self.regular_user1 = get_user_model().objects.create_superuser(
+        self.regular_user1 = get_user_model().objects.create_user(
             'regular_user1', 'regular_user1@test.test', 'regular_user1'
         )
-        self.regular_user2 = get_user_model().objects.create_superuser(
+        self.regular_user2 = get_user_model().objects.create_user(
             'regular_user2', 'regular_user2@test.test', 'regular_user2'
         )
 
     def test_when_user_is_superuser(self):
-        self.client = APIClient()
         self.client.login(username='super_user', password='super_user')
         url = '/api/records/' # TODO: reverse('record-create')
         domain = DomainFactory(name='example.com', owner=self.super_user)
@@ -99,7 +98,6 @@ class TestAddingRecords(BaseApiTestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_regular_user_owns_domain(self):
-        self.client = APIClient()
         self.client.login(username='regular_user1', password='regular_user1')
         url = '/api/records/' # TODO: reverse('record-create')
         domain = DomainFactory(name='example.com', owner=self.regular_user1)
@@ -113,7 +111,6 @@ class TestAddingRecords(BaseApiTestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_regular_user_not_own_domain(self):
-        self.client = APIClient()
         self.client.login(username='regular_user2', password='regular_user2')
         url = '/api/records/' # TODO: reverse('record-create')
         domain = DomainFactory(name='example.com', owner=self.regular_user2)
@@ -125,5 +122,4 @@ class TestAddingRecords(BaseApiTestCase):
         }
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        data = json.loads(response.content.decode('utf8'))
-        self.assertTrue(RecordRequest.objects.filter(record_id=data['id']))
+        self.assertTrue(RecordRequest.objects.filter(record_id=response.data['id']))
