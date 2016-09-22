2b84792fc1af66106d719ae5ee05f8ddd501971d
Tymoteusz Jankowski <tymoteusz.jankowski@gmail.com>
2016-09-22 08:54:12 +0200

    wip

--- dnsaas/urls.py
+++ dnsaas/urls.py
@@ -5,13 +5,7 @@ from django.conf import settings
 
 import powerdns
 from powerdns.utils import VERSION
-from powerdns.views import (
-    accept_domain_request,
-    accept_record_request,
-    accept_delete_request,
-    obtain_auth_token,
-    HomeView,
-)
+from powerdns.views import obtain_auth_token
 from ui.views import start_page
 
 
@@ -33,30 +27,5 @@ urlpatterns = patterns(
     url(r'^api-token-auth/', obtain_auth_token, name='get-api-token'),
     url(r'^api-docs/', include('rest_framework_swagger.urls')),
     url(r'^autocomplete/', include('autocomplete_light.urls')),
-    url(
-        r'^accept-domain/(?P<pk>[0-9]+)$',
-        accept_domain_request,
-        name='accept_domain'
-    ),
-    url(
-        r'^accept-record/(?P<pk>[0-9]+)$',
-        accept_record_request,
-        name='accept_record'
-    ),
-    url(
-        r'^accept-delete/(?P<pk>[0-9]+)$',
-        accept_delete_request,
-        name='accept_delete'
-    ),
+    url(r'^$', start_page, name='home'),
 )
-
-
-if settings.NEW_UI_ENABLED:
-    urlpatterns += (
-        url(r'^$', start_page, name='home'),
-    )
-else:
-    urlpatterns += (
-        url(r'^$', HomeView.as_view()),
-        url(r'^ui$', start_page, name='home'),
-    )
