dd9a0ee4be9d28de8ff92ba68dda02b476deebf1
Tymoteusz Jankowski <tymoteusz.jankowski@gmail.com>
2016-11-10 14:15:59 +0000

    mimgrations

--- powerdns/migrations/0033_auto_20161110_1515.py
+++ powerdns/migrations/0033_auto_20161110_1515.py
@@ -7,18 +7,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('powerdns', '0029_auto_20160922_0757'),
+        ('powerdns', '0032_auto_20161102_2006'),
     ]
 
     operations = [
         migrations.AddField(
             model_name='domain',
             name='require_sec_acceptance',
-            field=models.BooleanField(default=False, help_text='Do new A records require security acceptance'),
+            field=models.BooleanField(help_text='Do new A records require security acceptance', default=False),
         ),
         migrations.AddField(
             model_name='domain',
             name='require_seo_acceptance',
-            field=models.BooleanField(default=False, help_text='Does deleting A records require SEO acceptance'),
+            field=models.BooleanField(help_text='Does deleting A records require SEO acceptance', default=False),
         ),
     ]
