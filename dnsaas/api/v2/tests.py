6a7ead082e9dfab6a6673bdc3f5a8618dec2115c
Tymoteusz Jankowski <tymoteusz.jankowski@gmail.com>
2017-02-20 08:38:19 +0000

    wip

--- dnsaas/api/v2/tests.py
+++ dnsaas/api/v2/tests.py
@@ -699,7 +699,7 @@ class TestRecords(BaseApiTestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(
             response.data,
-            {'owner': ['Record requires owner to be editable. Please contact DNS support.']},  # noqa
+            {'owner': ['Record requires owner to be deletable. Please contact DNS support.']},  # noqa
         )
 
     def test_user_cant_delete_record(self):
